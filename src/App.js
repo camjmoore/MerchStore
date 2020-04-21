6c152c8883b6ab27a9b9d9407a90f6ea166b1b5e
Cameron Moore <cmrnmr@gmail.com>
2020-04-21 15:27:38 -0400

    started signinsignup comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css'
 import Header from './components/header/header.component'
+import signInsignUp from './pages/signIn-signUp/signIn-signUp.jsx'
 import Homepage from './pages/homepage/homepage'
 import ShopPage from './pages/shop/shop.component'
 import { Switch, Route } from 'react-router-dom';
@@ -12,6 +13,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={signInsignUp} />
+
       </Switch>
     </div>
   );
---
+++ src/pages/signIn-signUp/signIn-signUp.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './signIn-signUp.styles.scss'
+
+const signInsignUp = () => (
+  <div className='signIn-signUp'>SIGN IN</div>
+)
+
+export default signInsignUp;
